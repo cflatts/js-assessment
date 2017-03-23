fb15b0243cd18ff1cb090ece0067a849ec3f0a21
Caitlin Flattery <caitlinflattery@gmail.com>
2017-03-23 16:39:52 -0500

    completed arrays 'indexOf'

--- app/arrays.js
+++ app/arrays.js
@@ -2,7 +2,7 @@ exports = typeof window === 'undefined' ? global : window
 
 exports.arraysAnswers = {
   indexOf: function (arr, item) {
-
+    return arr.indexOf(item)
   },
 
   sum: function (arr) {
@@ -10,7 +10,6 @@ exports.arraysAnswers = {
   },
 
   remove: function (arr, item) {
-
   },
 
   removeWithoutCopy: function (arr, item) {
