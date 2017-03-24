95cfe97ed77b76a8eff4b38067854209212ef580
Caitlin Flattery <caitlinflattery@gmail.com>
2017-03-24 13:38:30 -0500

    removed console.log

--- app/flowControl.js
+++ app/flowControl.js
@@ -2,7 +2,6 @@ exports = typeof window === 'undefined' ? global : window
 
 exports.flowControlAnswers = {
   fizzBuzz: function (num) {
-    console.log(num)
     // write a function that receives a number as its argument;
     // if the number is divisible by 3, the function should return 'fizz';
     // if the number is divisible by 5, the function should return 'buzz';
