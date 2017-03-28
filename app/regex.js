d6f6a26b1e78a79a83deb18a70cdbf0d943d03a3
Caitlin Flattery <caitlinflattery@gmail.com>
2017-03-28 11:26:48 -0500

    completed regex 'containsNumber'

--- app/regex.js
+++ app/regex.js
@@ -2,7 +2,8 @@ exports = typeof window === 'undefined' ? global : window
 
 exports.regexAnswers = {
   containsNumber: function (str) {
-
+    var regEx = /\d/
+    return regEx.test(str)
   },
 
   containsRepeatingLetter: function (str) {
