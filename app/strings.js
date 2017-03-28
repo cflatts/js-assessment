ee40eb915f6631fe7c1106c157b6d001404fbd69
Caitlin Flattery <caitlinflattery@gmail.com>
2017-03-28 08:24:06 -0500

    completed strings 'reduceString'

--- app/strings.js
+++ app/strings.js
@@ -2,6 +2,20 @@ exports = typeof window === 'undefined' ? global : window
 
 exports.stringsAnswers = {
   reduceString: function (str, amount) {
+    var charArr = str.split('')
+    var shortStr = ''
+    var countObj = {}
+    for (var i = 0; i < charArr.length; i++) {
+      var char = charArr[i]
+      if (countObj[char] === undefined || (countObj[char] === amount && charArr[i] !== charArr[i - 1])) {
+        countObj[char] = 1
+        shortStr = shortStr + char
+      } else if (countObj[char] < amount) {
+        countObj[char] = countObj[char] + 1
+        shortStr = shortStr + char
+      }
+    }
+    return shortStr
   },
 
   wordWrap: function (str, cols) {
