6fbc89cc5899b86df568e75d674bf883cb6ff73b
Caitlin Flattery <caitlinflattery@gmail.com>
2017-03-28 10:13:35 -0500

    completed async 'async'

--- app/async.js
+++ app/async.js
@@ -2,7 +2,10 @@ exports = typeof window === 'undefined' ? global : window
 
 exports.asyncAnswers = {
   async: function (value) {
-
+    var promise = new Promise((resolve, reject) => {
+      resolve(value)
+    })
+    return promise
   },
 
   manipulateRemoteData: function (url) {
